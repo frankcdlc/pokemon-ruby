18c40446cea54d9af685518f8d74a6fdf1d53470
Alexis Parave <aparavev@gmail.com>
2022-02-25 13:51:09 -0500

    ADD: Class bot and advance menu

--- player.rb
+++ player.rb
@@ -1,11 +1,13 @@
 # require neccesary files
+require_relative "pokemon"
+require_relative "pokedex/pokemons"
 
 class Player
   # (Complete parameters)
   attr_reader :name, :pokemon
   def initialize(name,pokemon,pokemon_name)
     @name = name
-    @pokemon = 
+    @pokemon = Pokemon.new(pokemon_name, pokemon)
 
     # Complete this
   end
@@ -16,3 +18,19 @@ class Player
 end
 
 # Create a class Bot that inherits from Player and override the select_move method
+class Bot < Player
+  attr_reader :random_pokemon
+
+  def initialize
+    hash = Pokedex::POKEMONS
+    options = hash.collect do |key, value| 
+      key
+    end
+    @random_pokemon = options.sample
+
+    # super("Random Person",random_pokemon[:species],"")
+  end
+end
+
+bot = Bot.new
+p bot.random_pokemon
